ed605177939843773dc8101add141ee5e4a8857c
cjri <cjri@users.noreply.github.com>
2015-12-11 19:00:12 +0000

    Create utilities_sam.h

---
+++ utilities_sam.h
@@ -0,0 +1,19 @@
+//Shared information for linked optimisation
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+void GetOptions (run_params& p, int argc, const char **argv);
+void makequal (run_params& p, vector<char>& qual);
+int ScoreSim (string a, string b, int p, int rsize);
+int ScoreSim2 (string a, string b, int p, int rsize);
+string RevTr (string a);
+string RevString (string a);
+void PrintInstructions (int a);
