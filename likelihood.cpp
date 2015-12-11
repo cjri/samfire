855653719e96527c8572b45aa28971e899df44dc
cjri <cjri@users.noreply.github.com>
2015-12-11 18:54:14 +0000

    Create likelihood.cpp

---
+++ likelihood.cpp
@@ -0,0 +1,50 @@
+#include "shared.h"
+#include <iostream>
+#include <string>
+
+double DirichletMultiCalc (int N, double c, vector<int> obs, vector<double> inf, vector<double>& fact_store) {
+	double bin=0;
+	 for (unsigned int i=0;i<inf.size();i++) {
+		 if (inf[i]<1e-20) {
+			 inf[i]=1e-20;
+		 }
+	 }
+
+	if (N>0) {
+		bin=fact_store[N];
+		for (unsigned int i=0;i<obs.size();i++) {
+			bin=bin-fact_store[obs[i]];
+		}
+		vector<double> alpha;
+		for (unsigned int i=0;i<inf.size();i++) {
+			alpha.push_back(c*inf[i]);
+		}
+		double a=0;
+		for (unsigned int i=0;i<alpha.size();i++) {
+			a=a+alpha[i];
+			bin=bin-gsl_sf_lngamma(alpha[i]);
+		}
+		bin=bin+gsl_sf_lngamma(a);
+		a=0;
+		for (unsigned int i=0;i<alpha.size();i++) {
+			double b=alpha[i]+obs[i];
+			a=a+b;
+			bin=bin+gsl_sf_lngamma(b);
+		}
+		bin=bin-gsl_sf_lngamma(a);
+	} else {
+		bin=0;
+	}
+	
+//	cout << "L " << bin << "\n";
+	return(bin);
+}
+
+
+
+
+
+
+
+
+
