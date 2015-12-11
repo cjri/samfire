9a6dbe436b4699acb3b8ecf2071c89837f6e4288
cjri <cjri@users.noreply.github.com>
2015-12-11 18:50:55 +0000

    Create call_snps.h

---
+++ call_snps.h
@@ -0,0 +1,27 @@
+//Shared information for linked optimisation
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+//Call SNPs
+void CountNucleotides (run_params p, rseq refseq, vector<string> sam_files, vector< vector<joined> > t_reads, vector<nuc>& ref_counts);
+void SetupRefCounts (rseq refseq, vector<string> sam_files, vector<nuc>& ref_counts);
+void CountNucs (run_params p, rseq refseq, vector<joined> t_read, nuc& r_count);
+void SetupRCount (rseq refseq, nuc& r_count);
+
+void CallPolymorphisms (run_params p, rseq refseq, vector<nuc> ref_counts, vector<poly>& polys);
+void ProcessP (run_params p, int j, vector<double> freqs, vector<char> nuc, vector<int> counts, int N, vector<poly>& polys);
+void ConstructSLTrajs (run_params p, vector<poly> polys, vector<nuc> ref_counts, vector<str>& sltrajs);
+void SLTFreqs (run_params p, vector<str>& sltrajs);
+void SLTMeanFreqs (run_params p, vector<str>& sltrajs);
+void FilterSLTrajs (run_params p, vector<str>& sltrajs);
+void FilterSLTrajs2 (run_params p, vector<str>& sltrajs);
+void PotentialNonNeutrality (run_params p, double Csl_opt, vector<str>& sltrajs, vector<double> fact_store, gsl_rng *rgen);
+
