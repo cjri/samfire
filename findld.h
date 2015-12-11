90b070bef56f34595c034f3047096a9e77737a79
cjri <cjri@users.noreply.github.com>
2015-12-11 18:52:49 +0000

    Create findld.h

---
+++ findld.h
@@ -0,0 +1,15 @@
+//Shared information for linked optimisation
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+void CollectLDInfo (vector<str> sltrajs, vector<ld_info>& ld_data);
+void ProcessLDInfo (vector<double>& fact_store, vector<ld_info>& ld_data);
+
